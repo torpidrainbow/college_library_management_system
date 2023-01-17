40dd45919ce6dba2ba4c088ce6a3ec13da77e52e
Tanishq Patidar <tanishq_patidar@persistent.com>
2023-01-17 16:53:50 +0530

    first commit

---
+++ college-library-management-system/src/test/java/com/librarymanagement/collegelibrarymanagementsystem/CollegeLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarymanagement.collegelibrarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
